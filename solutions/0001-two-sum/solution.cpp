b9da2e0dabd31e48e39c4623ac039caffe19d171
adithyan s nair <adithyansnair2004@gmail.com>
2026-03-27 14:01:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.7 MB (58.53%)

--- solutions/0001-two-sum/solution.cpp
+++ solutions/0001-two-sum/solution.cpp
@@ -1,14 +1,22 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>un_map;
-        for(int i=0;i<nums.size();i++){
-            int complement = target - nums[i];
-            if(un_map.count(complement)){
-                return {un_map[complement],i};    
-            }
-            un_map[nums[i]] = i; 
-        }
-    return{};
-    }
-};
+            unordered_map<int, int> mp;
+
+                    for (int i = 0; i < nums.size(); i++) {
+                                int complement = target - nums[i];
+
+                                            if (mp.find(complement) != mp.end()) {
+                                                            return {mp[complement], i};
+                                                                        }
+
+                                                                                    mp[nums[i]] = i;
+                                                                                            }
+
+                                                                                                    return {};
+                                                                                                        }
+                                                                                                        };
