c48b0560dd2105092d79730ef73b4a4b7a310559
adithyan s nair <adithyansnair2004@gmail.com>
2026-06-14 19:34:19 +0000

    [LeetCode Sync] Runtime - 9 ms (34.42%), Memory - 13.3 MB (37.77%)

--- solutions/0013-roman-to-integer/solution.cpp
+++ solutions/0013-roman-to-integer/solution.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int>mp={
-            {'I',1},{'V',5},{'X',10},
-            {'L',50},{'C',100},{'D',500},
-           {'M',1000} 
+        std::unordered_map<char,int>table ={
+            {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
         };
-        int sum=0;
-    for (int i =0;i<(s.length());i++){
-        if(mp[s[i]]<mp[s[i+1]]){
-            sum=sum-(mp[s[i]]);
+    int total=table[s[s.size()-1]];
+        for(int i=s.size()-1;i>0;i--){
+            if(table[s[i-1]]>=table[s[i]]){
+                total+=table[s[i-1]];
+            }
+            else{
+                total-=table[s[i-1]];
+            }
         }
-        else{
-            sum=sum+mp[s[i]];
-        }
-
-    }
-return sum;
-
-
+    return total;
     }
 };
