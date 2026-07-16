c30d0d2acd3d91371ccb74edbea2ba8a8f8705a3
adithyan s nair <adithyansnair2004@gmail.com>
2026-07-16 06:04:55 +0000

    [LeetCode Sync] Runtime - 4 ms (36.54%), Memory - 43.3 MB (57.37%)

---
+++ solutions/0020-valid-parentheses/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='['||ch=='{')
+                stack.push(ch);
+            else if(!stack.isEmpty() && ((stack.peek()=='('&&ch==')')||(stack.peek()=='{'&&ch=='}')||(stack.peek()=='['&&ch==']')))
+                stack.pop();
+            else
+                return false;
+        }
+    if (stack.isEmpty())
+        return true;
+    else 
+        return false;
+    }
+}
