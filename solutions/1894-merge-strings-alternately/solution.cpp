3bee7b1bdd91d631e4e0316d3f9d41f746fe84d6
adithyan s nair <adithyansnair2004@gmail.com>
2026-07-27 14:50:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.8 MB (9.80%)

--- solutions/1894-merge-strings-alternately/solution.cpp
+++ solutions/1894-merge-strings-alternately/solution.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int i=0,j=0;
-        int n1= word1.size(),n2=word2.size();
-        string soln;
+        int s1=word1.length(),s2=word2.length();
+        int num=s1+s2;
+        std::string merged;
+        merged.reserve(num);
+        int m=0;
+        int n=0;
+        if(s1>=s2){
+            while(n<s2){
+                merged+=word1[m];
+                m++;
+                merged+=word2[n];
+                n++;
+            }
+            if(s1-s2!=0)
+                merged.append(word1,m,s1-m);
+            
+        }
+        else{
+            while(m<s1){
+                merged+=word1[m];
+                m++;
+                merged+=word2[n];
+                n++;
+            }
+            if(s2-s1!=0)
+                merged.append(word2,n,s2-m);
+            
+        }
+    return merged;
 
-        while (i<n1 && j<n2){
-            soln.push_back(word1[i++]);
-            soln.push_back(word2[j++]);
-        }
-        while  (i<n1){  soln.push_back(word1[i++]);
-        }
-        while  (j<n2){  soln.push_back(word2[j++]);
-        }
-    return soln;
     }
 };
