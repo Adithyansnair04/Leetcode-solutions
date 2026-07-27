5dd6ca60725f8418bdff0e35db7f492edf66b15a
adithyan s nair <adithyansnair2004@gmail.com>
2026-07-27 19:41:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 77.1 MB (75.75%)

---
+++ solutions/0002-add-two-numbers/solution.cpp
@@ -0,0 +1,32 @@
+struct Node {
+    int data;         
+    Node* next;         
+
+    Node(int val) : data(val), next(nullptr) {}
+};class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+   ListNode dummy(0); // Allocated on the stack to avoid dynamic allocation overhead
+        ListNode* current = &dummy;
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+        }
+
+        return dummy.next;
+    }
+
+};
