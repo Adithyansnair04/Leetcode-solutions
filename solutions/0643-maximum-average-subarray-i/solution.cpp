0830bd9bd5e4e0c108ad5610874ae7915943d76e
adithyan s nair <adithyansnair2004@gmail.com>
2026-08-04 19:03:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 113.8 MB (61.32%)

--- solutions/0643-maximum-average-subarray-i/solution.cpp
+++ solutions/0643-maximum-average-subarray-i/solution.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        int sum=0;
+        double sum=0;
+        if(k>nums.size())
+            return 0;
         for(int i=0;i<k;i++)
             sum+=nums[i];
-        int maxl=sum;
-        for (int i=k;i<nums.size();i++)
-        {
-            sum+=nums[i]-nums[i-k];
-            maxl=std::max(maxl,sum);
+        double maxr=sum;
+        for (int i=k;i<nums.size();i++){
+            int right = i;
+            int left=i-k;
+            sum+=nums[right];
+            sum-=nums[left];
+            maxr=std::max(sum,maxr);
         }
+    return maxr/k;
 
-return static_cast<double>(maxl) / k;}
+}
 
 };
