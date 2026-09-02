d6e78a866e05017a432b8ff30bb1e6cf2c098de0
adithyan s nair <adithyansnair2004@gmail.com>
2026-09-02 21:31:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.8 MB (93.04%)

--- solutions/0019-remove-nth-node-from-end-of-list/solution.cpp
+++ solutions/0019-remove-nth-node-from-end-of-list/solution.cpp
@@ -11,24 +11,16 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* dummy=new ListNode(0, head);
-        ListNode* fast=dummy;
-        ListNode* slow=dummy;
+        ListNode dummy(0, head);
+        ListNode* fast=&dummy;
+        ListNode* slow=&dummy;
         for(int i=0;i<n;i++)
             fast=fast->next;
-        while(fast->next!=nullptr){
+        while(fast!=nullptr && fast->next!=nullptr){
             fast=fast->next;
             slow=slow->next;
         }
-        ListNode* toDlt=slow->next;
         slow->next=slow->next->next;
-
-        delete toDlt;
-        
-    ListNode* reshd=dummy->next;
-        
-        
-    return reshd;
-
+    return dummy.next;
     }
 };
